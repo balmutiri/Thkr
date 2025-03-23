fc6ca5244a2cdf175070dfe33fe666d9f64b51d4
“Basma <itsbes566@gmail.com>
2025-03-23 05:01:52 +0300

    commit

--- api-server/index.js
+++ api-server/index.js
@@ -1,16 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const path = require('path');
-
-app.use(express.static(path.join(__dirname, '../dist/thkr')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/thkr/index.html'));
-});
-
-
-
 const app = express();
 app.use(bodyParser.json());
 
